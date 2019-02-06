2898ba0ce534cc436bf7258efe502ee559600241
veryspry <ehlinger.matt@gmail.com>
2019-02-05 20:59:03 -0600

    update .gitignore

--- index.js
+++ index.js
@@ -7,8 +7,50 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const path = require("path");
 const routes = require("./routes");
+const { google } = require("googleapis");
 // variables
 const port = 4000;
+
+var sheets = google.sheets("v4");
+
+authorize(function(authClient) {
+  var request = {
+    // The ID of the spreadsheet to retrieve data from.
+    spreadsheetId: process.env.GOOGLE_API_KEY, // TODO: Update placeholder value.
+    // The A1 notation of the values to retrieve.
+    range: "Sheet1", // TODO: Update placeholder value.
+    auth: authClient
+  };
+
+  sheets.spreadsheets.values.get(request, function(err, response) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    // TODO: Change code below to process the `response` object:
+    console.log(JSON.stringify(response, null, 2));
+  });
+});
+
+function authorize(callback) {
+  // TODO: Change placeholder below to generate authentication credentials. See
+  // https://developers.google.com/sheets/quickstart/nodejs#step_3_set_up_the_sample
+  //
+  // Authorize using one of the following scopes:
+  //   'https://www.googleapis.com/auth/drive'
+  //   'https://www.googleapis.com/auth/drive.file'
+  //   'https://www.googleapis.com/auth/drive.readonly'
+  //   'https://www.googleapis.com/auth/spreadsheets'
+  //   'https://www.googleapis.com/auth/spreadsheets.readonly'
+  var authClient = null;
+
+  if (authClient == null) {
+    console.log("authentication failed");
+    return;
+  }
+  callback(authClient);
+}
 
 // Set up middleware
 app.use(morgan("dev"));
